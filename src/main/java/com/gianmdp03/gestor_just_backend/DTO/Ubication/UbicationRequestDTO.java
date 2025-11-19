5c28526d43364d88dae817f81553af572bf69573
Gianluca Castorina <58142912+gianmdp03@users.noreply.github.com>
2025-11-19 17:54:03 -0300

    Add UbicationRequestDTO

--- src/main/java/com/gianmdp03/gestor_just_backend/DTO/Ubication/UbicationRequestDTO.java
+++ src/main/java/com/gianmdp03/gestor_just_backend/DTO/Ubication/UbicationRequestDTO.java
@@ -1,4 +1,14 @@
 package com.gianmdp03.gestor_just_backend.DTO.Ubication;
 
+import jakarta.validation.constraints.NotBlank;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class UbicationRequestDTO {
+    @NotBlank
+    private String name;
 }
