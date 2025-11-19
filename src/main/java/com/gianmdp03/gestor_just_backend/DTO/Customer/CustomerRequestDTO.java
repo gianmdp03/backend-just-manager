c81e7a1cda5707bb03f4367d45b906f5a5dd54ba
Gianluca Castorina <58142912+gianmdp03@users.noreply.github.com>
2025-11-19 17:37:36 -0300

    Add CustomerRequestDTO

--- src/main/java/com/gianmdp03/gestor_just_backend/DTO/Customer/CustomerRequestDTO.java
+++ src/main/java/com/gianmdp03/gestor_just_backend/DTO/Customer/CustomerRequestDTO.java
@@ -1,4 +1,20 @@
 package com.gianmdp03.gestor_just_backend.DTO.Customer;
 
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
 public class CustomerRequestDTO {
+    @NotBlank
+    private String name;
+
+    @NotBlank
+    private String lastname;
+
+    @NotBlank
+    private String phoneNumber;
 }
