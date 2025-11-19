0c13388afc317c72cfcc80ec5349e4153048ded3
Gianluca Castorina <58142912+gianmdp03@users.noreply.github.com>
2025-11-19 17:40:26 -0300

    Add InventoryItemRequestDTO

--- src/main/java/com/gianmdp03/gestor_just_backend/DTO/InventoryItem/InventoryItemRequestDTO.java
+++ src/main/java/com/gianmdp03/gestor_just_backend/DTO/InventoryItem/InventoryItemRequestDTO.java
@@ -1,4 +1,27 @@
 package com.gianmdp03.gestor_just_backend.DTO.InventoryItem;
 
+import jakarta.validation.constraints.FutureOrPresent;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@AllArgsConstructor
 public class InventoryItemRequestDTO {
+    @NotNull
+    private Long productId;
+
+    @NotNull
+    private Long ubicationId;
+
+    @NotNull
+    private int stock;
+
+    @NotNull
+    @FutureOrPresent
+    private LocalDate expireDate;
 }
