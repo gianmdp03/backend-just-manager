c545fce667497e2a5f6204344274944c27c8d720
Gianluca Castorina <58142912+gianmdp03@users.noreply.github.com>
2025-11-19 17:46:26 -0300

    Add OrderRequestDTO

--- src/main/java/com/gianmdp03/gestor_just_backend/DTO/Order/OrderRequestDTO.java
+++ src/main/java/com/gianmdp03/gestor_just_backend/DTO/Order/OrderRequestDTO.java
@@ -1,4 +1,33 @@
 package com.gianmdp03.gestor_just_backend.DTO.Order;
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PastOrPresent;
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class OrderRequestDTO {
+    @NotNull
+    private BigDecimal amount;
+
+    private String description;
+
+    @NotNull
+    @PastOrPresent
+    private LocalDate saleDate;
+
+    @NotNull
+    private Long customerId;
+
+    @NotNull
+    @NotEmpty
+    private List<Long> productIds;
 }
