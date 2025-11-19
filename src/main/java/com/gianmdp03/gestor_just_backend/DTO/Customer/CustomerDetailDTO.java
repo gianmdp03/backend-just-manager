c641b3c53e6327bbc50c51c422f319a9d3ff0588
Gianluca Castorina <58142912+gianmdp03@users.noreply.github.com>
2025-11-19 17:33:52 -0300

    Add CustomerDetailDTO

--- src/main/java/com/gianmdp03/gestor_just_backend/DTO/Customer/CustomerDetailDTO.java
+++ src/main/java/com/gianmdp03/gestor_just_backend/DTO/Customer/CustomerDetailDTO.java
@@ -1,4 +1,8 @@
 package com.gianmdp03.gestor_just_backend.DTO.Customer;
 
-public record CustomerDetailDTO() {
+import com.gianmdp03.gestor_just_backend.DTO.Order.OrderListDTO;
+
+import java.util.List;
+
+public record CustomerDetailDTO(Long id, String name, String lastname, String phoneNumber, List<OrderListDTO> orders) {
 }
