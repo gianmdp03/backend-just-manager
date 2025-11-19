6a7b8275bcfe88816a0d291699dad1dcaece989e
Gianluca Castorina <58142912+gianmdp03@users.noreply.github.com>
2025-11-19 17:17:15 -0300

    Add Order entity

--- src/main/java/com/gianmdp03/gestor_just_backend/Model/Order.java
+++ src/main/java/com/gianmdp03/gestor_just_backend/Model/Order.java
@@ -1,4 +1,52 @@
 package com.gianmdp03.gestor_just_backend.Model;
 
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "orders")
+@Getter
+@Setter
+@NoArgsConstructor
 public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, precision = 19, scale = 2)
+    private BigDecimal amount;
+
+    @Lob
+    private String description;
+
+    @Column(nullable = false)
+    private LocalDate saleDate;
+
+    @ManyToOne
+    @JoinColumn(name = "customer_id")
+    private Customer customer;
+
+    @ManyToMany
+    @JoinTable(
+            name = "order_products",
+            joinColumns = @JoinColumn(name = "order_id"),
+            inverseJoinColumns = @JoinColumn(name = "product_id")
+    )
+    private List<Product> products = new ArrayList<>();
+
+    @Builder
+    public Order(BigDecimal amount, String description, LocalDate saleDate, Customer customer) {
+        this.amount = amount;
+        this.description = description;
+        this.saleDate = saleDate;
+        this.customer = customer;
+    }
 }
