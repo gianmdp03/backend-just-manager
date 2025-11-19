05681b7b983d8cbc26058f459f9b61e30031bc15
Gianluca Castorina <58142912+gianmdp03@users.noreply.github.com>
2025-11-19 17:42:12 -0300

    Add OrderListDTO

--- src/main/java/com/gianmdp03/gestor_just_backend/DTO/Order/OrderListDTO.java
+++ src/main/java/com/gianmdp03/gestor_just_backend/DTO/Order/OrderListDTO.java
@@ -1,4 +1,9 @@
 package com.gianmdp03.gestor_just_backend.DTO.Order;
 
-public record OrderListDTO() {
+import com.gianmdp03.gestor_just_backend.Model.Customer;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+public record OrderListDTO(Long id, BigDecimal amount, LocalDate saleDate, Customer customer) {
 }
