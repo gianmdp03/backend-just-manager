14a8d532553ebd588b845fb3e5e8c3f365552955
Gianluca Castorina <58142912+gianmdp03@users.noreply.github.com>
2025-11-20 15:00:57 -0300

    Add InventoryItemService

--- src/main/java/com/gianmdp03/gestor_just_backend/Service/InventoryItemService.java
+++ src/main/java/com/gianmdp03/gestor_just_backend/Service/InventoryItemService.java
@@ -1,4 +1,16 @@
-package com.gianmdp03.gestor_just_backend.Service.Impl;
+package com.gianmdp03.gestor_just_backend.Service;
+
+import com.gianmdp03.gestor_just_backend.DTO.InventoryItem.InventoryItemListDTO;
+import com.gianmdp03.gestor_just_backend.DTO.InventoryItem.InventoryItemRequestDTO;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 public interface InventoryItemService {
+    InventoryItemListDTO addInventoryItem(InventoryItemRequestDTO inventoryItemRequestDTO);
+    Page<InventoryItemListDTO> listInventoryItems(Pageable pageable);
+    Page<InventoryItemListDTO> listInventoryItemsByProduct(Long productId, Pageable pageable);
+    Page<InventoryItemListDTO> listInventoryItemsByUbication(Long ubicationId, Pageable pageable);
+    Page<InventoryItemListDTO> listNonExpiredInventoryItems(Pageable pageable);
+    Page<InventoryItemListDTO> listExpiredInventoryItems(Pageable pageable);
+    void deleteInventoryItem(Long id);
 }
