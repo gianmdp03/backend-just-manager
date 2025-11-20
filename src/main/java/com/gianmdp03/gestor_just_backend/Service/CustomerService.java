6b6485e118c7795dabdfd19059b13ec1876d80a9
Gianluca Castorina <58142912+gianmdp03@users.noreply.github.com>
2025-11-20 14:52:25 -0300

    Add CustomerService

--- src/main/java/com/gianmdp03/gestor_just_backend/Service/CustomerService.java
+++ src/main/java/com/gianmdp03/gestor_just_backend/Service/CustomerService.java
@@ -1,4 +1,13 @@
-package com.gianmdp03.gestor_just_backend.Service.Impl;
+package com.gianmdp03.gestor_just_backend.Service;
+
+import com.gianmdp03.gestor_just_backend.DTO.Customer.CustomerDetailDTO;
+import com.gianmdp03.gestor_just_backend.DTO.Customer.CustomerListDTO;
+import com.gianmdp03.gestor_just_backend.DTO.Customer.CustomerRequestDTO;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 public interface CustomerService {
+    CustomerDetailDTO addCustomer(CustomerRequestDTO customerRequestDTO);
+    Page<CustomerListDTO> listCustomers(Pageable pageable);
+    void deleteCustomer(Long id);
 }
