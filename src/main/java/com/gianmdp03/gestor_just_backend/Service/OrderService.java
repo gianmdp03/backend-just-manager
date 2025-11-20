cc281bd967b5f2520e11f596a62c29a5f4aefbb3
Gianluca Castorina <58142912+gianmdp03@users.noreply.github.com>
2025-11-20 15:04:37 -0300

    Add OrderService

--- src/main/java/com/gianmdp03/gestor_just_backend/Service/OrderService.java
+++ src/main/java/com/gianmdp03/gestor_just_backend/Service/OrderService.java
@@ -1,4 +1,16 @@
-package com.gianmdp03.gestor_just_backend.Service.Impl;
+package com.gianmdp03.gestor_just_backend.Service;
+
+import com.gianmdp03.gestor_just_backend.DTO.Order.OrderDetailDTO;
+import com.gianmdp03.gestor_just_backend.DTO.Order.OrderListDTO;
+import com.gianmdp03.gestor_just_backend.DTO.Order.OrderRequestDTO;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.time.LocalDate;
 
 public interface OrderService {
+    OrderDetailDTO addOrder(OrderRequestDTO orderRequestDTO);
+    Page<OrderListDTO> listOrders(Pageable pageable);
+    Page<OrderListDTO> listOrdersBetween(LocalDate startDate, LocalDate endDate, Pageable pageable);
+    void deleteOrder(Long id);
 }
