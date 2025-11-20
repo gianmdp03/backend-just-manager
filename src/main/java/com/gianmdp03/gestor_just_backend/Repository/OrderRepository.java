f393a82fc51e3ee4c371a78f997212a955ccdae1
Gianluca Castorina <58142912+gianmdp03@users.noreply.github.com>
2025-11-20 14:27:43 -0300

    Add OrderRepository

--- src/main/java/com/gianmdp03/gestor_just_backend/Repository/OrderRepository.java
+++ src/main/java/com/gianmdp03/gestor_just_backend/Repository/OrderRepository.java
@@ -1,4 +1,13 @@
 package com.gianmdp03.gestor_just_backend.Repository;
 
-public interface OrderRepository {
+import com.gianmdp03.gestor_just_backend.Model.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Repository
+public interface OrderRepository extends JpaRepository<Order, Long> {
+    List<Order> findBySaleDateBetween(LocalDate startDate, LocalDate endDate);
 }
