9efd08f90b9cd4361b297f91fdcdb83fa48e3836
Gianluca Castorina <58142912+gianmdp03@users.noreply.github.com>
2025-11-20 14:26:22 -0300

    Add InventoryItemRepository

--- src/main/java/com/gianmdp03/gestor_just_backend/Repository/InventoryItemRepository.java
+++ src/main/java/com/gianmdp03/gestor_just_backend/Repository/InventoryItemRepository.java
@@ -1,4 +1,19 @@
 package com.gianmdp03.gestor_just_backend.Repository;
 
-public interface InventoryItemRepository {
+import com.gianmdp03.gestor_just_backend.Model.InventoryItem;
+import com.gianmdp03.gestor_just_backend.Model.Product;
+import com.gianmdp03.gestor_just_backend.Model.Ubication;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Repository
+public interface InventoryItemRepository extends JpaRepository<InventoryItem, Long> {
+    List<InventoryItem> findAllByProduct(Product product);
+    List<InventoryItem> findAllByUbication(Ubication ubication);
+    List<InventoryItem> findAllByOrderByExpireDateAsc();
+    List<InventoryItem> findByExpireDateBefore(LocalDate date);
+    List<InventoryItem> findByExpireDateGreaterThanEqual(LocalDate date);
 }
