34acdd3b1fcb0fe5fe0cbdeb7787f6b14c95bb20
Gianluca Castorina <58142912+gianmdp03@users.noreply.github.com>
2025-11-20 14:28:20 -0300

    Add ProductRepository

--- src/main/java/com/gianmdp03/gestor_just_backend/Repository/ProductRepository.java
+++ src/main/java/com/gianmdp03/gestor_just_backend/Repository/ProductRepository.java
@@ -1,4 +1,9 @@
 package com.gianmdp03.gestor_just_backend.Repository;
 
-public interface ProductRepository {
+import com.gianmdp03.gestor_just_backend.Model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
 }
