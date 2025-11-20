50da149a1f6226c46ae9d4dc47cfaf4bb5fcc394
Gianluca Castorina <58142912+gianmdp03@users.noreply.github.com>
2025-11-20 16:27:47 -0300

    Add ProductMapper

--- src/main/java/com/gianmdp03/gestor_just_backend/Mapper/ProductMapper.java
+++ src/main/java/com/gianmdp03/gestor_just_backend/Mapper/ProductMapper.java
@@ -1,4 +1,20 @@
 package com.gianmdp03.gestor_just_backend.Mapper;
 
+import com.gianmdp03.gestor_just_backend.DTO.Product.ProductDetailDTO;
+import com.gianmdp03.gestor_just_backend.DTO.Product.ProductListDTO;
+import com.gianmdp03.gestor_just_backend.DTO.Product.ProductRequestDTO;
+import com.gianmdp03.gestor_just_backend.Model.Product;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring", uses = {OrderMapper.class})
 public interface ProductMapper {
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "orders", ignore = true)
+    Product toEntity(ProductRequestDTO dto);
+    ProductDetailDTO toDetailDto(Product entity);
+    ProductListDTO toListDtoElement(Product entity);
+    List<ProductListDTO> toListDto(List<Product> entities);
 }
