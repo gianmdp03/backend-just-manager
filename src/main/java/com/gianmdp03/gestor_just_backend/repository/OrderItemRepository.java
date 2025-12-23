4c80169006e62a4eb43123229002a742e46bc484
Gianluca Castorina <58142912+gianmdp03@users.noreply.github.com>
2025-12-23 11:23:19 -0300

    Add OrderItemRepository

--- src/main/java/com/gianmdp03/gestor_just_backend/repository/OrderItemRepository.java
+++ src/main/java/com/gianmdp03/gestor_just_backend/repository/OrderItemRepository.java
@@ -1,4 +1,9 @@
 package com.gianmdp03.gestor_just_backend.repository;
 
-public interface OrderItemRepository {
+import com.gianmdp03.gestor_just_backend.model.OrderItem;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
 }
