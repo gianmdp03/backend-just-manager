f50af691c479a53e9cd8fdc6c617631d757a8a6c
Gianluca Castorina <58142912+gianmdp03@users.noreply.github.com>
2025-12-25 13:49:39 -0300

    Update InventoryItemListDTO.java

--- src/main/java/com/gianmdp03/gestor_just_backend/dto/inventoryitem/InventoryItemListDTO.java
+++ src/main/java/com/gianmdp03/gestor_just_backend/dto/inventoryitem/InventoryItemListDTO.java
@@ -1,9 +1,9 @@
 package com.gianmdp03.gestor_just_backend.dto.inventoryitem;
 
-import com.gianmdp03.gestor_just_backend.model.Product;
-import com.gianmdp03.gestor_just_backend.model.Location;
+import com.gianmdp03.gestor_just_backend.dto.location.LocationListDTO;
+import com.gianmdp03.gestor_just_backend.dto.product.ProductListDTO;
 
 import java.time.LocalDate;
 
-public record InventoryItemListDTO(Long id, Product product, Location location, int stock, LocalDate expireDate) {
+public record InventoryItemListDTO(Long id, ProductListDTO product, LocationListDTO location, int stock, LocalDate expireDate) {
 }
