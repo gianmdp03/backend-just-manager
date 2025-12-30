4cfb8e8a6011ffd88100d95b2fa0b7670d8051a4
Gianluca Castorina <58142912+gianmdp03@users.noreply.github.com>
2025-12-30 11:05:15 -0300

    Update ProductRequestDTO.java

--- src/main/java/com/gianmdp03/gestor_just_backend/dto/product/ProductRequestDTO.java
+++ src/main/java/com/gianmdp03/gestor_just_backend/dto/product/ProductRequestDTO.java
@@ -3,4 +3,4 @@ package com.gianmdp03.gestor_just_backend.dto.product;
 import jakarta.validation.constraints.NotBlank;
 
 public record ProductRequestDTO (@NotBlank String name,
-                                 String imageUrl){}
+                                 @NotBlank String imageUrl){}
